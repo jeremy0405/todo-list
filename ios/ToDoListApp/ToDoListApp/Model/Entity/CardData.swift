e11612553ebf887445463d27d236c583073db41e
Jihee hwang <hwangjihee94@gmail.com>
2022-04-13 11:16:55 +0900

    [feat] #29 카드 데이터 객체 및 HTTP 메서드 객체 구현

---
+++ ios/ToDoListApp/ToDoListApp/Model/Entity/CardData.swift
@@ -0,0 +1,19 @@
+//
+//  CardData.swift
+//  ToDoListApp
+//
+//  Created by Jihee hwang on 2022/04/12.
+//
+
+import Foundation
+
+struct CardData: Codable {
+    let id: Int
+    let userId: String
+    let title: String
+    let content: String
+    let sequence: Int
+    let status: Int
+}
+
+
